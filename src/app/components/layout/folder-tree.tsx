d35352dab910de78001435f52c0be42f0855f423
yuma <yuuma_0117@icloud.com>
2025-10-11 21:31:12 +0900

    feat: implement note creation functionality in folder tree

--- src/app/components/layout/folder-tree.tsx
+++ src/app/components/layout/folder-tree.tsx
@@ -2,26 +2,61 @@
 
 import { DeleteIcon } from "@/components/icon/delete-icon";
 import { Button } from "@/components/ui/button";
+import { useFolders } from "@/hooks/useFolders";
 import { useNotes } from "@/hooks/useNotes";
+import { Folder, Notebook } from "lucide-react";
 import Link from "next/link";
 import { useParams, useRouter } from "next/navigation";
+import { useState } from "react";
 
 export function FolderTree() {
   const { notes, createNote, deleteNote } = useNotes();
+  const [isCreatingNote, setIsCreatingNote] = useState(false);
+  const [newNoteTitle, setNewNoteTitle] = useState("");
+
   const { noteId } = useParams<{ noteId: string }>();
   const router = useRouter();
 
+  const handleCreateNote = async () => {
+    if (newNoteTitle.trim() === "") return;
+    const { note } = await createNote(newNoteTitle, "");
+    router.push(`/note/${note.id}`);
+    setIsCreatingNote(false);
+    setNewNoteTitle("");
+  };
+
   return (
     <div className="bg-gray-100 w-2xs">
-      <Button
-        onClick={async () => {
-          const { note } = await createNote("新しいノート", "");
-          router.push(`/note/${note.id}`);
-        }}
-      >
-        ノートを作成
-      </Button>
+      <div>
+        <Button variant="ghost" onClick={() => setIsCreatingNote(true)}>
+          <Notebook />
+        </Button>
+      </div>
       <ul>
+        {isCreatingNote && (
+          <li>
+            <input
+              type="text"
+              value={newNoteTitle}
+              onChange={(e) => setNewNoteTitle(e.target.value)}
+              onKeyDown={(e) => {
+                if (e.key === "Enter") {
+                  handleCreateNote();
+                }
+                if (e.key === "Escape") {
+                  setIsCreatingNote(false);
+                  setNewNoteTitle("");
+                }
+              }}
+              onBlur={() => {
+                setIsCreatingNote(false);
+                setNewNoteTitle("");
+              }}
+              autoFocus
+              className="w-full"
+            />
+          </li>
+        )}
         {notes.map((note) => (
           <li key={note.id} className="flex items-center justify-between">
             <Button variant="ghost">

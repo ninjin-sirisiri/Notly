3105377d3e8170b1b56ad63d809fa8c176924158
yuma <yuuma_0117@icloud.com>
2025-10-26 20:11:25 +0900

    feat: shadcn/ui導入完了

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 import { defineConfig } from 'vite';
 
 const host = process.env.TAURI_DEV_HOST;
@@ -7,6 +8,11 @@ const host = process.env.TAURI_DEV_HOST;
 export default defineConfig(async () => ({
   plugins: [react(), tailwindcss()],
   clearScreen: false,
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  },
   server: {
     port: 1420,
     strictPort: true,

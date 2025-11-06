f7f91353acd05db940662e4460483a232b21bc35
yuma <yuuma_0117@icloud.com>
2025-11-06 19:59:15 +0900

    feat: 自動保存機能完了

--- src/components/editor.tsx
+++ src/components/editor.tsx
@@ -44,6 +44,7 @@ export function Editor() {
             className="w-full text-2xl md:text-4xl font-black leading-tight tracking-[-0.033em] text-primary dark:text-white p-0.5"
             value={title}
             onChange={e => setTitle(e.target.value)}
+            onBlur={handleSave}
           />
           <span className="text-xs text-gray-400 dark:text-gray-500">
             {currentNote?.created_at.toLocaleString()}
@@ -108,6 +109,7 @@ export function Editor() {
                 placeholder="ここにノートを入力..."
                 value={content}
                 onChange={e => setContent(e.target.value)}
+                onBlur={handleSave}
               />
             </div>
           </div>
